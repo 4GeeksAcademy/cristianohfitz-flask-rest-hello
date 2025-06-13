bf1e6221e52922e17287cda0f9924189e3303aa8
Cristiano <higino@bu.edu>
2025-06-13 23:39:21 +0000

    pushyyyy

--- src/models.py
+++ src/models.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = "User"
+class user(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(15), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(15), unique=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -21,34 +21,34 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-class Post(db.Model):
-    __tablename__ = "Post"
+class post(db.Model):
+    __tablename__ = "post"
     id: Mapped[int] = mapped_column(primary_key=True)
-    caption: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    caption: Mapped[str] = mapped_column(String(150), unique=True)
     img: Mapped[str] = mapped_column(String(50), nullable=False)
     user_ID: Mapped[int] = mapped_column(ForeignKey("User.id"), nullable=False)
 
-class Comment(db.Model):
-    __tablename__ = "Comment"
+class comment(db.Model):
+    __tablename__ = "comment"
     id: Mapped[int] = mapped_column(primary_key=True)
-    text: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    text: Mapped[str] = mapped_column(String(150), unique=True)
     post_ID: Mapped[int] = mapped_column(ForeignKey("Post.id"), nullable=False)
     user_ID: Mapped[int] = mapped_column(ForeignKey("User.id"), nullable=False) 
 
-class Like(db.Model):
-    __tablename__ = "Like"
+class like(db.Model):
+    __tablename__ = "like"
     id: Mapped[int] = mapped_column(primary_key=True)
     post_ID: Mapped[int] = mapped_column(ForeignKey("Post.id"), nullable=False)
     user_ID: Mapped[int] = mapped_column(ForeignKey("User.id"), nullable=False) 
 
-class Story(db.Model):
-    __tablename__ = "Story"
+class story(db.Model):
+    __tablename__ = "story"
     id: Mapped[int] = mapped_column(primary_key=True)
     post_ID: Mapped[int] = mapped_column(ForeignKey("Post.id"), nullable=False)
     user_ID: Mapped[int] = mapped_column(ForeignKey("User.id"), nullable=False)
 
-class Share(db.Model):
-    __tablename__ = "Share"
+class share(db.Model):
+    __tablename__ = "share"
     id: Mapped[int] = mapped_column(primary_key=True)
     story_ID: Mapped[int] = mapped_column(ForeignKey("Story.id"), nullable=False)
     post_ID: Mapped[int] = mapped_column(ForeignKey("Post.id"), nullable=False)
